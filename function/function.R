2961df0b79ee0784eced8b1929be760565cc3589
Jonathan Smith <jonathan@peoplesized.com>
2020-02-17 10:33:15 +0200

    tidy, bump version, add debug flag

--- function/function.R
+++ function/function.R
@@ -1,7 +1,7 @@
 suppressPackageStartupMessages(library(sf))
 library(geojsonio)
 library(disarmr)
-  
+
 function(params) {
     
     set.seed(1981)
@@ -9,11 +9,13 @@ function(params) {
     point_data <- st_read(as.json(params$point_data), quiet = TRUE)
     
     # Run function
-    res <- prevalence_predictor_mgcv(point_data = point_data,
-                              layer_names = params$layer_names,
-                              exceedance_threshold = params$exceedance_threshold,
-                              batch_size = params$batch_size,
-                              uncertainty_fieldname = params$uncertainty_fieldname)
+    res <- prevalence_predictor_mgcv(
+        point_data = point_data,
+        layer_names = params$layer_names,
+        exceedance_threshold = params$exceedance_threshold,
+        batch_size = params$batch_size,
+        uncertainty_fieldname = params$uncertainty_fieldname
+    )
     
     # package and return
     return(geojson_list(res))
